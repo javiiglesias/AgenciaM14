10f2751926ad2f11aee24f59bad2e566f6f4392e
Unknown <ivan.mir.014@gmail.com>
2017-03-21 16:14:45 +0100

    Eliminación de catergorías para Directores

--- view/directors.php
+++ view/directors.php
@@ -5,34 +5,6 @@ require_once 'view/menuEdicio.php';
     <div class="container">
         <div class="col-xs-12 col-md-6 col-md-push-3">
             <h1>PAGINA DIRECTORS</h1>
-            <form action="?ctl=director" method="post">
-                <div class="form-group">
-                    Cercar per Categoria:
-                    <?php
-                    // modificar según las catergorias de directors?
-                    require_once 'model/categoria.class.php';
-                    $cat = new CategoriaDAO();
-
-                    $sel = $cat->createSelectCategories();
-                    echo $sel;
-                    ?> 
-                    Quantitat de registres:
-                    <select name="quantitat">
-                        <option value=" "> </option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="10">10</option>
-                        <option value="12">12</option>
-                        <option value="16">16</option>
-                        <option value="24">24</option>
-                        <option value="36">36</option>
-                        <option value="72">72</option>
-                        <option value="100">100</option>
-                        <option value="200">200</option>
-                    </select>
-                    <button name="Submit" class="btn btn-info"><image class="btn-icon" src="view/images/cercar.png"/>  Cercar</button>
-                </div>            
-            </form>
         </div>
         <div class="row col-lg-8 col-lg-push-3 ">
             <?php if (count($directors) > 0) { ?>

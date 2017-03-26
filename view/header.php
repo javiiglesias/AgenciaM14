00c858b5ce14b1db5480d513f7e193fb002a7256
Aburdene <yaritzaaburdene30@gmail.com>
2017-03-26 02:47:27 +0100

    Modificacio de Plantilla i Vistes Obra

--- view/header.php
+++ view/header.php
@@ -3,7 +3,7 @@
         <title><?php echo $titlePage ?></title>
         <!-- Estils -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!--<link rel="stylesheet" type="text/css" href="view/css/tema.css"/>--->
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css"/>-
         <link rel="stylesheet" type="text/css" href="view/css/estil.css"/>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
          <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css">
